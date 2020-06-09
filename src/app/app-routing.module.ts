ac9eb6e531d79233c854ebb7246a7b7076610eef
nvantan2 <tannguyen12.07.sk@gmail.com>
2020-06-09 16:53:29 +0700

    80%

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LevelDetailComponent } from './level-detail/level-detail.component';
+import { ResultComponent } from './result/result.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'level/:id',
+    component: LevelDetailComponent,
+  },
+  {
+    path: 'result',
+    component: ResultComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
